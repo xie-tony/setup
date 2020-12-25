5890ed00a827d9fdf2c579643024794a1a0bd78b
Tony Xie <tony.xie@uwaterloo.ca>
2020-12-25 15:27:45 -0500

    delete files

--- brew.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Use linuxbrew to download some stuff that's not in apt-get
-CI=1 /bin/bash -c \
-    "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-brew install fzf ripgrep
