d846d0ff3dc99fa09a4f7e4ef95e14b500c5a3aa
Tony Xie <tony.xie@uwaterloo.ca>
2020-12-25 15:27:01 -0500

    First version

--- setup.sh
+++ setup.sh
@@ -1,6 +1,51 @@
 #!/bin/bash
 
-non_sudo="sudo -u $SUDO_USER"
+non_sudo="sudo -u $SUDO_USER bash -c"
+
+# Pull dotfile from repo
+installDotfile () {
+    config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+    git clone --bare https://github.com/xie-tony/dotfiles.git $HOME/.cfg
+
+    # Backup the defaults
+    mkdir -p .config-backup && \
+    $config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
+    xargs -I{} mv {} .config-backup/{}
+
+    $config checkout
+    $config config --local status.showUntrackedFiles no
+}
+
+# Use linuxbrew to download some stuff that's not in apt-get
+installBrew () {
+    CI=1 /bin/bash -c \
+        "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    /home/linuxbrew/.linuxbrew/bin/brew install\
+        fzf ripgrep
+}
+
+installOcaml () {
+    # It's already in the shell file copied from dotfile
+    opam init -n
+    eval $(opam env)
+    opam switch create 4.10.0
+    eval $(opam env)
+
+    opam install -y \
+        utop core async \
+        merlin ocamlformat \
+        ocaml-lsp-server
+}
+
+installZsh () {
+    sh -c "$(curl -fsSL \
+        https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" \
+        "" \
+        --keep-zshrc
+}
+
+declare -f
 
 # Update apt-get
 apt-get update
@@ -11,15 +56,14 @@ apt-get install -y git
 
 # Install zsh
 apt-get install -y zsh
-$non_sudo bash ./zsh.sh
 
 # Dotfile stuff
-$non_sudo bash ./dotfile.sh
+$non_sudo "installDotfile"
 
 # Linuxbrew
-$non_sudo bash ./brew.sh
+$non_sudo "installBrew"
 
 # Ocaml stuff
 apt-get install -y opam
-$non_sudo bash ./ocaml.sh
+$non_sudo "installOcaml"
 
