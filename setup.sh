768f807d00dc0b64fade6144cec3922cb7beb0d8
Tony Xie <tony.xie@uwaterloo.ca>
2020-12-26 21:19:38 -0500

    Add $HOME to config

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ installDotfile () {
     # Backup the defaults
     mkdir -p .config-backup && \
     $config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
-    xargs -I{} mv {} .config-backup/{}
+    xargs -I{} mv $HOME/{} $HOME/.config-backup/{}
 
     $config checkout
     $config config --local status.showUntrackedFiles no
